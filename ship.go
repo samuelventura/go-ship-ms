66af9fc7fc68e1cb61e1c6087dc351fffbcc0b59
Samuel Ventura <samuel.ventura@yeico.com>
2021-11-13 21:11:56 -0600

    Single endpoint loop

--- ship.go
+++ ship.go
@@ -41,98 +41,102 @@ func run(node tree.Node) {
 	} else {
 		txts = []string{pool}
 	}
-	rand.Seed(time.Now().UnixNano())
+	var eps []string
 	for _, txt := range txts {
 		addrs := strings.Split(txt, ",")
-		l := len(addrs)
-		//random start
-		n := rand.Intn(l)
-		for i := 0; i < l; i++ {
-			addr := addrs[(n+i)%l]
-			log.Println(addr, name)
-			conn, err := net.DialTimeout("tcp", addr, 10*time.Second)
-			if err != nil {
-				log.Println(err)
-				continue
-			}
-			keepAlive(conn)
-			sshCon, sshch, reqch, err := ssh.NewClientConn(conn, addr, config)
-			if err != nil {
-				log.Println(err)
-				conn.Close()
-				continue
-			}
-			node.AddCloser("conn", conn.Close)
-			node.AddCloser("sshCon", sshCon.Close)
-			node.AddProcess("ping", func() {
-				defer log.Println("request handler exited")
-				for {
-					timer := time.NewTimer(10 * time.Second)
-					select {
-					case req := <-reqch:
-						if req == nil {
+		eps = append(eps, addrs...)
+	}
+	log.Println("eps", eps)
+	rand.Seed(time.Now().UnixNano())
+	l := len(eps)
+	//random start
+	n := rand.Intn(l)
+	for i := 0; i < l; i++ {
+		ii := (n + i) % l
+		addr := eps[ii]
+		log.Println(ii, addr, name)
+		conn, err := net.DialTimeout("tcp", addr, 10*time.Second)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		keepAlive(conn)
+		sshCon, sshch, reqch, err := ssh.NewClientConn(conn, addr, config)
+		if err != nil {
+			log.Println(err)
+			conn.Close()
+			continue
+		}
+		node.AddCloser("conn", conn.Close)
+		node.AddCloser("sshCon", sshCon.Close)
+		node.AddProcess("ping", func() {
+			defer log.Println("request handler exited")
+			for {
+				timer := time.NewTimer(10 * time.Second)
+				select {
+				case req := <-reqch:
+					if req == nil {
+						return
+					}
+					if req.Type == "ping" {
+						err := req.Reply(true, nil)
+						switch err {
+						case nil:
+							timer.Stop()
+						default:
 							return
 						}
-						if req.Type == "ping" {
-							err := req.Reply(true, nil)
-							switch err {
-							case nil:
-								timer.Stop()
-							default:
-								return
-							}
-						}
-					case <-timer.C:
-						log.Println("idle timeout")
-						return
-					case <-node.Closed():
-						return
 					}
-				}
-			})
-			handleForward := func(ch ssh.NewChannel) {
-				addr := string(ch.ExtraData())
-				log.Println("open", addr)
-				defer log.Println("close", addr)
-				sshch, _, err := ch.Accept()
-				if err != nil {
-					log.Println(err)
+				case <-timer.C:
+					log.Println("idle timeout")
 					return
-				}
-				defer sshch.Close()
-				conn, err := net.DialTimeout("tcp", addr, 5*time.Second)
-				if err != nil {
-					log.Println(err)
-					return
-				}
-				defer conn.Close()
-				keepAlive(conn)
-				done := make(chan interface{}, 2)
-				go func() {
-					io.Copy(sshch, conn)
-					done <- true
-				}()
-				go func() {
-					io.Copy(conn, sshch)
-					done <- true
-				}()
-				select {
-				case <-done: //close on first error
 				case <-node.Closed():
+					return
 				}
 			}
-			node.AddProcess("sshch", func() {
-				defer log.Println("channel handler exited")
-				for ch := range sshch {
-					if ch.ChannelType() != "forward" {
-						ch.Reject(ssh.Prohibited, "unsupported")
-						return
-					}
-					go handleForward(ch)
-				}
-			})
-			return
+		})
+		handleForward := func(ch ssh.NewChannel) {
+			addr := string(ch.ExtraData())
+			log.Println("open", addr)
+			defer log.Println("close", addr)
+			sshch, _, err := ch.Accept()
+			if err != nil {
+				log.Println(err)
+				return
+			}
+			defer sshch.Close()
+			conn, err := net.DialTimeout("tcp", addr, 5*time.Second)
+			if err != nil {
+				log.Println(err)
+				return
+			}
+			defer conn.Close()
+			keepAlive(conn)
+			done := make(chan interface{}, 2)
+			go func() {
+				io.Copy(sshch, conn)
+				done <- true
+			}()
+			go func() {
+				io.Copy(conn, sshch)
+				done <- true
+			}()
+			select {
+			case <-done: //close on first error
+			case <-node.Closed():
+			}
 		}
+		node.AddProcess("sshch", func() {
+			defer log.Println("channel handler exited")
+			for ch := range sshch {
+				if ch.ChannelType() != "forward" {
+					ch.Reject(ssh.Prohibited, "unsupported")
+					return
+				}
+				go handleForward(ch)
+			}
+		})
+		return
 	}
 	log.Fatalln("connection failed", txts)
 }
